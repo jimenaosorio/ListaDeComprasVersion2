5e5cd807c8db769b468f15fd4dea6fb475d5c723
jimenaosorio <jimena.b.osorio@gmail.com>
2019-10-18 19:58:30 -0300

    Lista de compras, operaciones CRUD con ArrayList, previo a SQLite.

---
+++ app/src/main/java/cl/ejercicios/listadecomprasversion2/NuevoProductoActivity.java
@@ -0,0 +1,63 @@
+package cl.ejercicios.listadecomprasversion2;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import cl.ejercicios.listadecomprasversion2.modelo.ListDeCompras;
+import cl.ejercicios.listadecomprasversion2.modelo.Producto;
+
+public class NuevoProductoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nuevo_producto);
+    }
+    public void ingresarProducto(View view)
+    {
+        String nombre=((TextView) findViewById(R.id.ingresarNombre)).getText().toString();
+        String cantidadStr=
+                ((TextView)findViewById(R.id.ingresarCantidad)).getText().toString();
+        String unidad=
+                ((Spinner)findViewById(R.id.ingresarUnidad)).getSelectedItem().toString();
+        String unidadNueva=
+                ((TextView)findViewById(R.id.otraUnidad)).getText().toString();
+        int cantidad=0;
+        try{
+            cantidad=Integer.parseInt(cantidadStr);
+        }catch (NumberFormatException ex)
+        {
+            Toast.makeText(this,"Debe ingresar un número en la cantidad",
+                    Toast.LENGTH_SHORT).show();
+        }
+        if(cantidad>0)
+        {
+            if(unidad.equals("Otro")){
+                unidad=unidadNueva;
+            }
+            Producto producto=new Producto(nombre,cantidad,unidad);
+            ListDeCompras listaDeCompras=ListDeCompras.getInstancia();
+            listaDeCompras.agregarProducto(producto);
+            finish();
+        }
+        else {
+            Toast.makeText(this,"Ingrese una cantidad mayor a cero",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+}
